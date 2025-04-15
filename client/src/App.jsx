0baa30c930c7e8af03c756a28fe561e6bd419fd7
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-04-16 01:45:40 +0300

    Navbar section update: Set up app routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-
+import Navbar from "./components/navbar.component.jsx"
+import {Route, Routes} from "react-router-dom";
 const App = () => {
     return (
-        <h1>Full Stack Blog App</h1>
-    )
+        <Routes>
+            <Route path="/" element={<Navbar />} >
+                <Route path="signin" element={<h1>Sign In Page </h1>} />
+                <Route path="signup" element={<h1>Sign Up Page </h1>} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
