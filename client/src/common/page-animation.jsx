dd767343d17afa9f8de0d5f9673c1bd5c9d17f68
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-04-17 20:44:10 +0300

    Page Animation with framer-motion

---
+++ client/src/common/page-animation.jsx
@@ -0,0 +1,14 @@
+import {AnimatePresence, motion} from "framer-motion";
+
+const AnimationWrapper = ({children, keyValue, className, initial = {opacity: 0}, animate = {opacity:1}, transition = {duration:1}}) =>{
+    return(
+        <AnimatePresence>
+            <motion.div initial={initial} animate={animate} transition={transition} key={keyValue}
+                        className={className}>
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    )
+}
+
+export default AnimationWrapper;
