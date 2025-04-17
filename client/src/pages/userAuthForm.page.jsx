2e9882bba308c81b0000d4ef368517ca25bc1c54
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-04-17 17:38:07 +0300

    UI Update: User Auth

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,6 @@
 import InputBox from "../components/input.component.jsx";
+import googleIcon from "../assets/google.png";
+import {Link} from "react-router-dom";
 
 const UserAuthForm = ({type}) => {
     return (
@@ -32,6 +34,38 @@ const UserAuthForm = ({type}) => {
                             placeholder="Password"
                             icon="fi-rr-key"
                         />
+
+                        <button className="btn-dark center mt-14" type="submit">
+                            {type}
+                        </button>
+
+                        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 text-black font-bold">
+                            <hr className="w-1/2 border-black"/>
+                            <p>or</p>
+                            <hr className="w-1/2 border-black"/>
+                        </div>
+
+                        <button className="btn-dark flex items-center justify-center gap-4 w-[70%] center">
+                            <img src= {googleIcon} className="w-5" alt="google icon"/>
+                            Continue with Google
+                        </button>
+
+                {
+                    type === "Login" ?
+                        <p className="mt-6 text-dark-grey text-xl text-center">
+                            Don't have an account?
+                            <Link to="/signup" className="underline text-black text-xl ml-1">
+                                Join us today
+                            </Link>
+                        </p>
+                        :
+                        <p className="mt-6 text-dark-grey text-xl text-center">
+                            Already a member?
+                            <Link to="/login" className="underline text-black text-xl ml-1">
+                                Sign in
+                            </Link>
+                        </p>
+                }
             </form>
         </section>
     )
