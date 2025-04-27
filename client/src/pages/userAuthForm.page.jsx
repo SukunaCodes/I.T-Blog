18135914d9a9fa3039a46434538d3e132d2fc1ab
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-04-27 12:23:37 +0300

    Frontend Update: Saving user sessions in React States

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -1,23 +1,42 @@
 import InputBox from "../components/input.component.jsx";
 import googleIcon from "../assets/google.png";
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import AnimationWrapper from "../common/page-animation.jsx";
-import {useRef} from "react";
 import {Toaster, toast} from "react-hot-toast";
+import axios from "axios";
+import {storeSession} from "../common/session.jsx";
+import {useContext} from "react";
+import {UserContext} from "../App.jsx";
 
 const UserAuthForm = ({type}) => {
 
-    const authForm = useRef();
+    let {userAuth: {access_token}, setUserAuth} = useContext(UserContext)
+
+    console.log(access_token);
+
+    // Function to make Requests to the Backend Server
+    const userAuthServerConnection = (serverRoute, currentFormData) => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, currentFormData)
+            .then(({ data }) => {
+                storeSession("user", JSON.stringify(data))
+                setUserAuth(data)
+            })
+            .catch(({response}) => {
+                toast.error(response.data.error)
+            })
+    }
 
     const handleSubmit = (e) => {
         //Prevent null submissions
         e.preventDefault();
 
+        let serverRoute = type === "Login" ? "/auth/signin" : "/auth/signup";
+
         let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
         let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
         // Handle raw form data
-        let form = new FormData(authForm.current);
+        let form = new FormData(formElement);
         let currentFormData = {};
 
         // Loop through the form data (**Security reasons)
@@ -42,76 +61,95 @@ const UserAuthForm = ({type}) => {
             return toast.error("Password should be 6 to 20 characters long with 1 numeric, 1 lowercase, 1 Uppercase letters")
         }
         console.log(currentFormData);
+
+        userAuthServerConnection(serverRoute, currentFormData);
+
     }
+
+    /*// Google
+    const handleGoogleAuth = (e) => {
+        e.preventDefault();
+        authWithGoogle().then(user => {
+            let serverRoute = "/auth/google";
+            let currentFormData = {
+                access_token: user.access_token
+            }
+        })
+            .catch(err => {
+                toast.error('Trouble login through Google');
+                return console.log(err);
+            })
+    }*/
+
     return (
-        <AnimationWrapper keyValue={type}>
-            <section className="h-cover flex items-center justify-center">
-                <Toaster />
-                <form ref={authForm} className="w-[80%] max-w-[400px]">
-                    <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-                        {type === "Login" ? "Welcome Back" : "Join us today"}
-                    </h1>
+        access_token ? <Navigate to="/" /> :
+            <AnimationWrapper keyValue={type}>
+                <section className="h-cover flex items-center justify-center">
+                    <Toaster/>
+                    <form id="formElement" className="w-[80%] max-w-[400px]">
+                        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+                            {type === "Login" ? "Welcome Back" : "Join us today"}
+                        </h1>
 
-                    {
-                        type !== "Login" ?
-                            <InputBox
-                                name="fullname"
-                                type="text"
-                                placeholder="Full Name"
-                                icon="fi-rr-user"
-                            />
-                            : ""
-                    }
+                        {
+                            type !== "Login" ?
+                                <InputBox
+                                    name="fullname"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    icon="fi-rr-user"
+                                />
+                                : ""
+                        }
 
-                    <InputBox
-                        name="email"
-                        type="email"
-                        placeholder="Email Address"
-                        icon="fi-rr-envelope"
-                    />
+                        <InputBox
+                            name="email"
+                            type="email"
+                            placeholder="Email Address"
+                            icon="fi-rr-envelope"
+                        />
 
-                    <InputBox
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        icon="fi-rr-key"
-                    />
+                        <InputBox
+                            name="password"
+                            type="password"
+                            placeholder="Password"
+                            icon="fi-rr-key"
+                        />
 
-                    <button className="btn-dark center mt-14" type="submit" onClick={handleSubmit}>
-                        {type}
-                    </button>
+                        <button className="btn-dark center mt-14" type="submit" onClick={handleSubmit}>
+                            {type}
+                        </button>
 
-                    <div className="relative w-full flex items-center gap-2 my-10 opacity-10 text-black font-bold">
-                        <hr className="w-1/2 border-black"/>
-                        <p>or</p>
-                        <hr className="w-1/2 border-black"/>
-                    </div>
+                        <div className="relative w-full flex items-center gap-2 my-10 opacity-10 text-black font-bold">
+                            <hr className="w-1/2 border-black"/>
+                            <p>or</p>
+                            <hr className="w-1/2 border-black"/>
+                        </div>
 
-                    <button className="btn-dark flex items-center justify-center gap-4 w-[70%] center">
-                        <img src={googleIcon} className="w-5" alt="google icon"/>
-                        Continue with Google
-                    </button>
+                        <button className="btn-dark flex items-center justify-center gap-4 w-[70%] center">
+                            <img src={googleIcon} className="w-5" alt="google icon"/>
+                            Continue with Google
+                        </button>
 
-                    {
-                        type === "Login" ?
-                            <p className="mt-6 text-dark-grey text-xl text-center">
-                                Don't have an account?
-                                <Link to="/signup" className="underline text-black text-xl ml-1">
-                                    Join us today
-                                </Link>
-                            </p>
-                            :
-                            <p className="mt-6 text-dark-grey text-xl text-center">
-                                Already a member?
-                                <Link to="/login" className="underline text-black text-xl ml-1">
-                                    Sign in
-                                </Link>
-                            </p>
-                    }
-                </form>
-            </section>
-        </AnimationWrapper>
-
+                        {
+                            type === "Login" ?
+                                <p className="mt-6 text-dark-grey text-xl text-center">
+                                    Don't have an account?
+                                    <Link to="/signup" className="underline text-black text-xl ml-1">
+                                        Join us today
+                                    </Link>
+                                </p>
+                                :
+                                <p className="mt-6 text-dark-grey text-xl text-center">
+                                    Already a member?
+                                    <Link to="/login" className="underline text-black text-xl ml-1">
+                                        Sign in
+                                    </Link>
+                                </p>
+                        }
+                    </form>
+                </section>
+            </AnimationWrapper>
     )
 }
 
