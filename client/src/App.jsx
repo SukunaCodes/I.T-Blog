2c58c015efb4ebb566a769621754dd3f41305f6e
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-04-29 21:37:16 +0300

    Frontend Update: Added Editor Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import UserAuthForm from "./pages/userAuthForm.page.jsx";
 import {createContext, useEffect, useState} from "react";
 import {lookInSession} from "./common/session.jsx";
+import Editor from "./pages/editor.pages.jsx";
 
 export const UserContext = createContext({});
 
@@ -20,6 +21,7 @@ const App = () => {
         <UserContext.Provider value={{userAuth, setUserAuth}}>
             <Routes>
                 <Route path="/" element={<Navbar/>}>
+                <Route path="/editor" element={<Editor />}/>
                     <Route path="login" element={<UserAuthForm type={"Login"}/>}/>
                     <Route path="signup" element={<UserAuthForm type={"Sign Up"}/>}/>
                 </Route>
