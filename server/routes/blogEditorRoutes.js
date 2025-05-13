7938432c3c292f8d2be5073e26ed6a15f36144fe
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-05-13 21:24:38 +0300

    Backend Update: create blog and update routes.

--- server/routes/blogEditorRoutes.js
+++ server/routes/blogEditorRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import {upload_url} from "../controllers/blogEditorUploadURL.js";
+import {createBlog} from "../controllers/createBlogController.js";
+import {verifyJWT} from "../middlewares/userAuth.js";
 
 let router = express.Router();
 
 router.get('/upload/get-image-url', upload_url);
+router.post('/create', verifyJWT, createBlog);
 
 export default router;
