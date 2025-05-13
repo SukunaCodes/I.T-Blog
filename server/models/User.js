b986a7df21f62ece28e1d1773b3ee0ea85ccd8ff
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-05-13 21:16:31 +0300

    Backend Update: Added associations to user with Blog.

--- server/models/User.js
+++ server/models/User.js
@@ -1,106 +1,119 @@
 import {DataTypes, Model} from 'sequelize';
-import 'dotenv/config'
-import sequelize from "../config/database.js";
-
-
 const profile_imgs_name_list = ["Aizen", "Ayanakoji", "Ichigo", "Urahara", "Sukuna", "Dante", "Byakuya", "Rukia"];
 const profile_imgs_collections_list = ["notionists-neutral", "adventurer-villain", "fun-isekai", "overpowered-shounen", "raging-vessel", "dark-manipulator"];
 
-const User = sequelize.define('user', {
-    id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-    },
-    fullname: {
-        type: DataTypes.STRING,
-        required: true,
-        validate: {
-            len: [3, 255], // minlength: 3
-            toLowerCase(value) {
-                if (value) this.setDataValue('fullname', value.toLowerCase()); // toLowerCase: true
+export default (sequelize) => {
+    class User extends Model {
+    }
+
+    User.init(
+        {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER,
+            },
+            fullname: {
+                type: DataTypes.STRING,
+                allowNull: false, // Changed from required: true to align with Sequelize syntax
+                validate: {
+                    len: [3, 255],
+                    toLowerCase(value) {
+                        if (value) this.setDataValue('fullname', value.toLowerCase());
+                    },
+                },
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true,
+                    toLowerCase(value) {
+                        if (value) this.setDataValue('email', value.toLowerCase());
+                    },
+                },
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: true, // Password can be null for Google Auth users
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                unique: true,
+                validate: {
+                    len: [3, 255],
+                },
+            },
+            bio: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue: '',
+                validate: {
+                    len: [0, 200],
+                },
+            },
+            profile_img: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                defaultValue: () => {
+                    return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`;
+                },
+            },
+            account_details: {
+                type: DataTypes.JSONB,
+                allowNull: true,
+                defaultValue: {
+                    total_posts: 0,
+                    total_reads: 0,
+                },
+            },
+            social_links: {
+                type: DataTypes.JSONB,
+                allowNull: true,
+                defaultValue: [
+                    {platform: 'youtube', url: ''},
+                    {platform: 'instagram', url: ''},
+                    {platform: 'x', url: ''},
+                    {platform: 'substack', url: ''},
+                    {platform: 'github', url: ''},
+                    {platform: 'medium', url: ''},
+                    {platform: 'website', url: ''},
+                ],
+            },
+
+            google_auth: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
             },
         },
-    },
-    email: {
-        type: DataTypes.STRING,
-        unique: true,
-        required: true,
-        validate: {
-            isEmail: true, //validate email format
-            toLowerCase(value) {
-                if (value) this.setDataValue('email', value.toLowerCase());
-            },
-        },
-    },
-    password: {
-        type: DataTypes.STRING,
-    },
-    username: {
-        type: DataTypes.STRING,
-        unique: true,
-        validate: {
-            len: [3, 255],
-        },
-    },
-    bio: {
-        type: DataTypes.STRING,
-        defaultValue: "",
-        validate: {
-            len: [0, 200], //maxlength:200
-        },
-    },
-    profile_img: {
-        type: DataTypes.STRING,
-        defaultValue: () => {
-            return `https://api.dicebear.com/6.x/${profile_imgs_collections_list[Math.floor(Math.random() * profile_imgs_collections_list.length)]}/svg?seed=${profile_imgs_name_list[Math.floor(Math.random() * profile_imgs_name_list.length)]}`;
-        },
-    },
-    social_links: {
-        type: DataTypes.JSONB,
-        allowNull: true,
-        defaultValue: [
-            {platform: 'youtube', url: ''},
-            {platform: 'instagram', url: ''},
-            {platform: 'x', url: ''},
-            {platform: 'substack', url: ''},
-            {platform: 'github', url: ''},
-            {platform: 'medium', url: ''},
-            {platform: 'website', url: ''},
-        ]
-    },
-    account_details: {
-        type: DataTypes.JSONB,
-        allowNull: true,
-        defaultValue: {
-            total_posts: 0,
-            total_reads: 0,
-        },
-    },
-    // Google Auth
-    google_auth: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
-    createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-    },
-    updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
-    },
-}, {timestamps: true},);
+        {
+            sequelize,
+            modelName: 'User',
+            tableName: 'users',
+        }
+    );
 
-/*// Define Blog associations outside sequelize.define
-User.associate = (models) => {
-    User.hasMany(models.Blog, {
-        foreignKey: 'userId',
-        as: 'blogs',
-    });
-}*/
+    // Define associations as a static method
+    User.associate = (models) => {
+        User.hasMany(models.Blog, {
+            foreignKey: 'userId',
+            as: 'blogs',
+        });
+    };
 
-export default User;
+    return User;
+};
