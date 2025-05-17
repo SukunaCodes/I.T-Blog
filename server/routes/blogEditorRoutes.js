055fb9a575932853b67fdbe3671b9e0291f24e2d
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-05-17 23:36:45 +0300

    Backend Update: GET request.

--- server/routes/blogEditorRoutes.js
+++ server/routes/blogEditorRoutes.js
@@ -2,10 +2,18 @@ import express from 'express';
 import {upload_url} from "../controllers/blogEditorUploadURL.js";
 import {createBlog} from "../controllers/createBlogController.js";
 import {verifyJWT} from "../middlewares/userAuth.js";
+import urlFetchLinkPreview from "../controllers/fetchURLPreview.js";
+import {fetchLatestBlogs} from "../controllers/fetchLatestPublishedBlogs.js";
 
 let router = express.Router();
 
+// Link Preview Route
+router.get('/fetch-link-preview', urlFetchLinkPreview);
 router.get('/upload/get-image-url', upload_url);
+router.get('/latest', fetchLatestBlogs);
+
+
 router.post('/create', verifyJWT, createBlog);
+
 
 export default router;
