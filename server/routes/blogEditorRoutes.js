226444c021b1dc218bdfba746611594bb19d4d49
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-05-21 20:34:59 +0300

    Backend Update: trending blog route.

--- server/routes/blogEditorRoutes.js
+++ server/routes/blogEditorRoutes.js
@@ -4,6 +4,7 @@ import {createBlog} from "../controllers/createBlogController.js";
 import {verifyJWT} from "../middlewares/userAuth.js";
 import urlFetchLinkPreview from "../controllers/fetchURLPreview.js";
 import {fetchLatestBlogs} from "../controllers/fetchLatestPublishedBlogs.js";
+import {fetchTrendingBlogs} from "../controllers/fetchTrendingPublishedBlogs.js";
 
 let router = express.Router();
 
@@ -11,6 +12,7 @@ let router = express.Router();
 router.get('/fetch-link-preview', urlFetchLinkPreview);
 router.get('/upload/get-image-url', upload_url);
 router.get('/latest', fetchLatestBlogs);
+router.get('/trending', fetchTrendingBlogs);
 
 
 router.post('/create', verifyJWT, createBlog);
