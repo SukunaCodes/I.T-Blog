64216438789d424d8ef6603ec75599a456cccb3c
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-06-05 14:02:23 +0300

    Backend Update: filter blogs by category.

---
+++ server/controllers/filterBlogsByTags.js
@@ -0,0 +1,36 @@
+import {models} from "../config/database.js";
+import {Op} from "sequelize";
+
+const {Blog, User} = models;
+
+export const filterBlogsByTags = async (req, res) => {
+    let{tag} = req.body;
+
+    // Validate Tag
+    if(!tag || typeof tag !== 'string'){
+        return res.status(400).json({error: "Tag must be a non-empty string"});
+    }
+    let findQuery = {
+        tags: {[Op.contains]: Array.isArray(tag) ? tag : [tag]},
+        draft: false,
+    };
+
+    let maxLimit = 5;
+
+    try{
+        const blogs = await Blog.findAll({
+            where: findQuery,
+            include: {
+                model: User,
+                as: 'user',
+                attributes: ['fullname', 'profile_img', 'username'],
+            },
+            order: [['createdAt', 'DESC']],
+            attributes: ['id', 'title', 'description', 'banner', 'activity', 'tags', 'createdAt'],
+            limit: maxLimit,
+        })
+        return res.status(200).json({blogs});
+    } catch (err){
+        return res.status(500).json({error: err.message});
+    }
+};
