211de8bef23c7545055af809bb04df203d0c87db
Forbidden Programmer <67829913+SukunaCodes@users.noreply.github.com>
2025-06-11 11:00:22 +0300

    Backend Update: Search implementation by tags.

--- server/controllers/filterBlogsByTags.js
+++ server/controllers/filterBlogsByTags.js
@@ -4,35 +4,37 @@ import {Op} from "sequelize";
 const {Blog, User} = models;
 
 export const filterBlogsByTags = async (req, res) => {
-    let{tag, page} = req.body;
+    let {tag, query, page} = req.body;
 
-    // Validate Tag
-    if(!tag || typeof tag !== 'string'){
-        return res.status(400).json({error: "Tag must be a non-empty string"});
-    }
-    let findQuery = {
-        tags: {[Op.contains]: Array.isArray(tag) ? tag : [tag]},
-        draft: false,
-    };
 
-    let maxLimit = 2;
-    let skip = (page - 1) * maxLimit;
-
-    try{
-        const blogs = await Blog.findAll({
-            where: findQuery,
-            include: {
-                model: User,
-                as: 'user',
-                attributes: ['fullname', 'profile_img', 'username'],
-            },
-            order: [['createdAt', 'DESC']],
-            attributes: ['id', 'title', 'description', 'banner', 'activity', 'tags', 'createdAt'],
-            limit: maxLimit,
-            offset: skip,
-        })
-        return res.status(200).json({blogs});
-    } catch (err){
-        return res.status(500).json({error: err.message});
+    // Validate input
+    if (!(!query && (!tag || typeof tag !== "string"))) {
+        let findQuery = {draft: false};
+        if (tag) {
+            findQuery.tags = {[Op.contains]: Array.isArray(tag) ? tag : [tag]};
+        } else if (query) {
+            findQuery.title = {[Op.iLike]: `%${query}%`};
+        }
+        let maxLimit = 5;
+        let skip = (parseInt(page) - 1) * maxLimit || 0;
+        try {
+            const blogs = await Blog.findAll({
+                where: findQuery,
+                include: {
+                    model: User,
+                    as: 'user',
+                    attributes: ['fullname', 'profile_img', 'username'],
+                },
+                order: [['createdAt', 'DESC']],
+                attributes: ['id', 'title', 'description', 'banner', 'activity', 'tags', 'createdAt'],
+                limit: maxLimit,
+                offset: skip,
+            })
+            return res.status(200).json({blogs});
+        } catch (err) {
+            return res.status(500).json({error: err.message});
+        }
+    } else {
+        return res.status(400).json({error: "Query or tag must be provided and tag must be a string"});
     }
 };
--- server/middlewares/searchBlogsCountByCategory.js
+++ server/middlewares/searchBlogsCountByCategory.js
@@ -5,20 +5,23 @@ const {Blog} = models;
 
 export const searchBlogsCountByCategory = async (req, res) => {
     try {
-        let {tag} = req.body;
-        if(!tag || typeof tag !== "string"){
-            return res.status(400).json({error: "Tag must be a non-empty string"});
-        }
-        let findQuery = {
-            tags: {[Op.contains] : [tag]},
-            draft: false,
-        };
 
-        let count = await Blog.count({
-            where: findQuery,
-        });
-        return res.status(200).json({totalDocs: count});
-    } catch (err){
+        let {tag, query} = req.body;
+        if (!(!query && (!tag || typeof tag !== "string"))) {
+            let findQuery = {draft: false};
+            if (tag) {
+                findQuery.tags = {[Op.contains]: Array.isArray(tag) ? tag : [tag]};
+            } else if (query) {
+                findQuery.title = {[Op.iLike]: `%${query}%`};
+            }
+            let count = await Blog.count({
+                where: findQuery,
+            });
+            return res.status(200).json({totalDocs: count});
+        } else {
+            return res.status(400).json({error: "Query or tag must be provided and tag must be a string"});
+        }
+    } catch (err) {
         console.log(err.message);
         return res.status(500).json({error: err.message});
     }
